c8952d266716838e67161ca5a340349a5b3d3b5b
sontung <tung.nguyenson1404@gmail.com>
2018-04-02 09:50:52 +0200

    bubblesort

--- buble_sort/gpu_sort.cu
+++ buble_sort/gpu_sort.cu
@@ -5,13 +5,16 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
+#include <stdio.h>
 
-#define NB_THREADS 2
+#define NB_THREADS 1024
+#define NB_NUMBERS 200
+#define NB_EXPERIMENTS 100
 
 
 void print_vector(int* array, int k) {
-  for(int i=0; i < k; i++) {
-    printf("%d ", array[k]);
+  for(size_t i=0; i < k; i++) {
+    printf("%d ", array[i]);
   }
   printf("\n");
 }
@@ -19,7 +22,7 @@ void print_vector(int* array, int k) {
 bool compare_vectors(int *v1, std::vector<int> v2) {
   for(int i=0; i < v2.size(); i++) {
     if (v1[i] != v2[i]) {
-      std::cout << v1[i] << '\n';
+      printf("wrong at %d %d vs %d\n", i, v1[i], v2[i]);
       return false;
     }
   }
@@ -34,59 +37,74 @@ std::vector<int> create_vector(int N) {
   return res;
 }
 
+
 __global__
 void bubble_sort(int *A, int n) {
+  __shared__ int end[NB_THREADS]; // termination condition for each thread
+  __shared__ int race[NB_NUMBERS]; // race condition for each element of input array
+  for (int u=0; u<NB_THREADS; u++) { end[u] = 0; }
+  for (int v=0; v<NB_NUMBERS; v++) { race[v] = 1; }
   int temp;
   int index = threadIdx.x;
-  int index2 = index+1;
-  __shared__ int end[1];
-  __shared__ int all_phases[NB_THREADS];
-  printf("sorting\n");
-  while (end[0] != 1) {
-    printf("sorting2\n");
-    if ((index == 0) || (all_phases[index] == 1)) {
-      end[0] = 1;
-      printf("sorting3\n");
-      for (int i=0; i<n-index; i++) {
-        if (A[i-1] > A[i]) {
-          temp = A[i-1];
-          A[i-1] = A[i];
-          A[i] = temp;
-          end[0] = 0;
-        }
-        if (i > 1 && all_phases[index2] != 1) {
-          all_phases[index2] = 1;
+  while (1) {
+    end[index] = 1;
+    for (int i=0; i<n; i++) {
+      while (1) {
+        if ((race[i] == 1 && race[i-1] == 1) || (i == 0 && race[i] == 1)) {
+          if (A[i-1] > A[i]) {
+            // Block race condition
+            race[i] = 0;
+            race[i-1] = 0;
+
+            // Swap
+            temp = A[i-1];
+            A[i-1] = A[i];
+            A[i] = temp;
+
+            // Release race condition
+            end[index] = 0;
+            race[i] = 1;
+            race[i-1] = 1;
+          }
+          break;
         }
       }
-      for(int i=0; i < n; i++) {
-        printf("%d ", A[i]);
-      }
-      printf("\n");
     }
+    if (end[index] == 1) { break; }
   }
 }
 
 int main(void) {
-  int range = 50;
-  std::vector<int> v = create_vector(range);
-  std::vector<int> v_orig = create_vector(range);
-  std::random_shuffle ( v.begin(), v.end() );
-  std::random_shuffle ( v.begin(), v.end() );
+  for (int e=0; e<NB_EXPERIMENTS; e++) {
+    // Init random input array
+    int range = NB_NUMBERS;
+    std::vector<int> v = create_vector(range);
+    std::vector<int> v_orig = create_vector(range);
+    std::random_shuffle ( v.begin(), v.end() );
+    std::random_shuffle ( v.begin(), v.end() );
 
-  int *x;
-  cudaMallocManaged(&x, range*sizeof(int));
-  for (int i = 0; i < range; i++) {
-    x[i] = v[i];
+    int *x;
+    cudaMallocManaged(&x, range*sizeof(int));
+    for (int i = 0; i < range; i++) {
+      x[i] = v[i];
+      printf("%d %d\n", x[i], v[i]);
+    }
+    print_vector(x, range);
+
+    // Sort
+    bubble_sort<<<1, NB_THREADS>>>(x, range);
+
+    cudaDeviceSynchronize();
+
+    // Check output
+    if (!compare_vectors(x, v_orig)) {
+      printf("%s\n", "Wrong algorithm");
+      print_vector(x, range);
+      break;
+    } else { printf("%s %d\n", "True algorithm", e); }
+
+    // Free memory
+    cudaFree(x);
   }
-
-  bubble_sort<<<1, NB_THREADS>>>(x, range);
-
-  cudaDeviceSynchronize();
-
-  if (!compare_vectors(x, v_orig)) {
-    printf("%s\n", "Wrong algorithm");
-  } else { printf("%s\n", "True algorithm"); }
-
-  // Free memory
-  cudaFree(x);
 }
+
